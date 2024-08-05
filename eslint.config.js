32111fb8a6003585f55845031e37c701a3f427d4
Vienxom5 <tanvien2712@gmail.com>
2024-08-05 23:29:25 +0700

    update_code

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import { Linter } from "eslint";
+
+export default [
+  {
+    files: ["*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+    },
+  },
+];
---
+++ index.js
@@ -0,0 +1,4 @@
+import {launchApp} from "./src/app/Application.js";
+
+// Launch our application
+launchApp();
